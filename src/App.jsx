b44330b2eceaf6bea45b42c23104c7022ddfdc4e
mibichoandafugao <augustohenao1234@gmail.com>
2025-09-12 19:54:25 -0500

    Tailwind añadido, react router dom añadido

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline text-amber-400'>Prueba de tailwind</h1>
+    </div>
+  )
 }
 
 export default App
